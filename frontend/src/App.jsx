971cd42dc1e6f9cf95d1656e3a0b7590c48ead97
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-18 16:46:11 +0600

    added react-icons, header, footer and remove some unwanted images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 import './App.css';
+
 
 function App() {
   return (
-    <div className='bg-red-600'>This is app</div>
+    <>
+      <Header />
+      <div className='bg-red-600'>This is app</div>
+      <Footer />
+    </>
   )
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,5 @@
+export function Header() {
+    return (
+        <div>This is header</div>
+    )
+}
